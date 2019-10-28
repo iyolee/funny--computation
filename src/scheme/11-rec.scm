e22adbb2c3507dc608cc80bea1b3bcb00840d959
lizhongjian <lizhongjian@qianxin.com>
2019-10-28 13:45:24 +0800

    feat: 第一章

--- src/scheme/11-rec.scm
+++
@@ -1,9 +0,0 @@
-#lang planet neil/sicp
-(define (f n)
-    (if (< n 3)
-        n
-        (+ (f (- n 1))
-           (* 2 (f (- n 2)))
-           (* 3 (f (- n 3))))))
-
-(f 6)
---
+++ src/scheme/lambda.scm
@@ -0,0 +1,6 @@
+(define plus4 (lambda (x) (+ x 4)))
+
+;等价于
+;(define (plus4 x) (+ x 4))
+
+(plus4 8)
